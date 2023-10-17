32d85abe4fdce5f6dfc8be21085d0280b3226031
RishiRagu2510 <136676056+RishiRagu2510@users.noreply.github.com>
2023-10-17 21:19:15 +0530

    ES_uploaded

---
+++ exp3.ino
@@ -0,0 +1,16 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+void setup()
+{
+  lcd.begin(16,2);
+  lcd.clear();
+
+}
+void loop(){
+  lcd.setCursor(5,0);
+  lcd.print("Embedded");
+  lcd.setCursor(6,1);
+  lcd.print("System");
+  delay(1000);
+}
+
